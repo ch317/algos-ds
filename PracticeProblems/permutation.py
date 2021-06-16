77fd2cb2e2e2dca7facf359d765350d4bcaaa0df
ch3 <poleadornato@gmail.com>
2021-06-16 18:49:51 +0200

    new exercise: permutation.py

---
+++ PracticeProblems/permutation.py
@@ -0,0 +1,20 @@
+def permutation(strInput):
+    if len(strInput) == 0:
+        return [""]
+    elif len(strInput) == 1:
+        return [strInput]
+    
+    prefix = strInput[0]
+    permutationsWithoutPrefix = permutation(strInput[1:])
+
+    listPermutations = []
+    for permutationWithoutPrefix in permutationsWithoutPrefix:
+        for i in range(len(permutationWithoutPrefix) + 1):
+            copyPermutation = permutationWithoutPrefix[:]
+            newPermutation =  copyPermutation[:i] + prefix + copyPermutation[i:]
+            listPermutations.append(newPermutation)
+    
+    return listPermutations
+
+permutations = permutation("abc")
+print(permutations)
